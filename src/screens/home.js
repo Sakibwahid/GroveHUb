8f4c307d59764a5848d343abe07e3312938a1ba6
Sakib Wahid <sakibruns@gmail.com>
2023-12-15 05:22:50 +0600

    Auth and spotify.js

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,25 +1,46 @@
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Library from './library';  // Adjust the path
-import Feed from './feed';  // Adjust the path
-import Trending from './trending';  // Adjust the path
-import Player from './player';  // Adjust the path
-import Favourite from './favourite';  // Adjust the path
-import './home.css';
-import Sidebar from '../components/sidebar';
+import React, { useState, useEffect } from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Sidebar from "../../components/sidebar";
+import { setClientToken } from "../../spotify";
+import Login from "../auth/login";
+import Favorites from "../favorites";
+import Feed from "../feed";
+import Library from "../library";
+import Player from "../player";
+import Trending from "../trending";
+import "./home.css";
 
 export default function Home() {
-  return (
+  const [token, setToken] = useState("");
+
+  useEffect(() => {
+    const token = window.localStorage.getItem("token");
+    const hash = window.location.hash;
+    window.location.hash = "";
+    if (!token && hash) {
+      const _token = hash.split("&")[0].split("=")[1];
+      window.localStorage.setItem("token", _token);
+      setToken(_token);
+      setClientToken(_token);
+    } else {
+      setToken(token);
+      setClientToken(token);
+    }
+  }, []);
+
+  return !token ? (
+    <Login />
+  ) : (
     <Router>
       <div className="main-body">
-        <Sidebar/>
-      <Routes>
-        <Route path="/" element={<Library />} />
-        <Route path="/feed" element={<Feed />} />
-        <Route path="/trending" element={<Trending />} />
-        <Route path="/player" element={<Player />} />
-        <Route path="/favourite" element={<Favourite />} />
-      </Routes>
+        <Sidebar />
+        <Routes>
+          <Route path="/" element={<Library />} />
+          <Route path="/feed" element={<Feed />} />
+          <Route path="/trending" element={<Trending />} />
+          <Route path="/player" element={<Player />} />
+          <Route path="/favorites" element={<Favorites />} />
+        </Routes>
       </div>
     </Router>
   );
