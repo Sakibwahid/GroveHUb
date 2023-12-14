3f5649859743d4d8f88015f86f62b4ea95503767
Sakib Wahid <sakibruns@gmail.com>
2023-12-14 23:14:42 +0600

    9th min

---
+++ src/screens/home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Library from './library';  // Adjust the path
+import Feed from './feed';  // Adjust the path
+import Trending from './trending';  // Adjust the path
+import Player from './player';  // Adjust the path
+import Favourite from './favourite';  // Adjust the path
+
+export default function Home() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Library />} />
+        <Route path="/feed" element={<Feed />} />
+        <Route path="/trending" element={<Trending />} />
+        <Route path="/player" element={<Player />} />
+        <Route path="/favourite" element={<Favourite />} />
+      </Routes>
+    </Router>
+  );
+}
