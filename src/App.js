f9148aee24c4b568d8ea41d6ddc93745a1eecba9
Sakib Wahid <sakibruns@gmail.com>
2023-12-15 07:41:50 +0600

    last try

--- src/App.js
+++ src/App.js
@@ -1,11 +1,10 @@
-
-import React from 'react'
-import Home from "./screens/home"
+import React from "react";
+import Home from "./screens/home";
 
 export default function App() {
   return (
     <div>
-   <Home/>
+      <Home />
     </div>
-  )
+  );
 }
